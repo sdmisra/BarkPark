b8a74fc04d2d53ade949a90ce60043f909562af0
Shane Misra <shane.misra@gmail.com>
2023-06-08 22:57:21 -0600

    almost back to where i was

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,55 @@
 import {Switch, Route} from 'react-router-dom'
 import React, {useState, useEffect} from 'react'
-import HomePage from '../Body/HomePage';
+import HomePage from '../HomePage/HomePage';
+import SearchForm from '../SearchForm/SearchForm';
 
 import './App.css';
 
 const App = () => {
+  const [displayedDog, setDisplayedDog] = useState('')
+
+  const getRandomDog = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      if (!response.ok) {
+        throw new Error(`Error in Random Call! code: ${response.status}`)
+      }
+      const randomDog = await response.json()
+      setDisplayedDog(randomDog.message)
+      return randomDog;
+    } catch(error) {
+      return error
+    }
+  }
+
+  const getSpecificBreed = async (breed) => {
+    console.log(breed)
+    try {
+      const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+      if (!response.ok) {
+        throw new Error(`Error in Random Call! code: ${response.status}`)
+      }
+      const oneOfABreed = await response.json()
+      console.log(oneOfABreed.message)
+      setDisplayedDog(oneOfABreed.message)
+      return oneOfABreed;
+    } catch(error) {
+      return error
+    }
+  }
+
+  useEffect(()=> {
+    if (displayedDog === '') {
+      getRandomDog()
+    }
+  },[displayedDog])
+
 
   return (
     <Switch>
       <Route path='/'>
-        <HomePage />
+        <HomePage filepath={displayedDog} getRandomDog={getRandomDog}/>
+        <SearchForm getSpecificBreed={getSpecificBreed}/>
       </Route>
       <Route path='/saved'>
       </Route>
---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = ({filepath, getRandomDog}) => {
+
+  const randomPathBreed = filepath.split('/')[4]
+
+  return (
+    <div className='random-dog-window'>
+      <img className='random-dog-image' src={filepath}/>
+      <h3>For more, search for: {randomPathBreed}</h3>
+      <button className='random-dog-button' onClick={() => getRandomDog()}>Click Here for a new Pooch!</button>
+    </div>
+  )
+}
+
+export default HomePage;
